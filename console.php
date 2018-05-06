bc9cfc03220e8ece6fedb0c32beb7a107935e4e9
Armen Hovhannisyan <armen.hov24@gmail.com>
2018-05-06 22:53:00 +0300

    githubUsers

--- console.php
+++ console.php
@@ -4,22 +4,27 @@ require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/ConnectionClass.php';
 require_once __DIR__.'/ConnectDatabaseException.php';
 
+use GitHub\ConnectDatabaseException;
+use GitHub\ConnectionClass as Connection;
+use GuzzleHttp\Client;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use GuzzleHttp\Client;
-use GitHub\ConnectionClass as Connection;
-use GitHub\ConnectDatabaseException;
 
 $app = new Application('GitHub Users', '1.0.0');
 $app->register('github-users')
+    ->addOption('host', null, InputOption::VALUE_REQUIRED, 'Database Host')
+    ->addOption('user', null, InputOption::VALUE_REQUIRED, 'Database User')
+    ->addOption('password', null, InputOption::VALUE_REQUIRED, 'Database Password')
+    ->addOption('database', null, InputOption::VALUE_REQUIRED, 'Database Name')
     ->setCode(function (InputInterface $input, OutputInterface $output) {
         try {
             $client = new Client();
             $response = $client->get('https://api.github.com/users');
             $users = json_decode($response->getBody()->getContents(), true);
 
-            $connection = new Connection("localhost","root","12345","github");
+            $connection = new Connection($input->getOption('host'),$input->getOption('user'),$input->getOption('password'),$input->getOption('database'));
             if (!$connection->createUserTable()) {
                 $output->writeln("Warning: `user` table already exists.");
             }
@@ -33,10 +38,8 @@ $app->register('github-users')
                     if (!$connection->updateUser($user['id'], $user['login'])) {
                         $output->writeln(sprintf("Error: Cannot update user by githubID %s.", $user['id']));
                     }
-                } else {
-                    if (!$connection->crateUser($user['id'], $user['login'])) {
-                        $output->writeln(sprintf("Error: Cannot create user by githubID %s.", $user['id']));
-                    }
+                } elseif (!$connection->crateUser($user['id'], $user['login'])) {
+                    $output->writeln(sprintf("Error: Cannot create user by githubID %s.", $user['id']));
                 }
             }
         } catch (ConnectDatabaseException $exception) {
@@ -46,8 +49,7 @@ $app->register('github-users')
         }
 
 
-        return $output->writeln("<info>Hello Github!</info>");
+        return $output->writeln("<info>Github users successfully added!</info>");
     })
     ->getApplication()
     ->run();
-
